7861f6ed9ae6e34b85dd8291dea3656fc63c1964
Yogita Kapgate <yogitakapgate2006@gmail.com>
2026-04-12 13:22:40 +0530

    arithmatic operations

---
+++ assignment1/Mathmatics4.java
@@ -0,0 +1,25 @@
+/*john and his three freindswent on trip.for they travelled 200 km .a vwhicle has mielage of 15 kilometr per liter a fuel.
+ fuel coat pe liter rs 90.*/
+
+ public class Mathmatics4{
+       public static void main(String[] args){
+
+       int distance = 200 ;
+       int fuel_liter = 90;
+       int mielage = 15;
+
+      int required_fuel = 200/15;
+      int total_cost = (required_fuel*90);
+      int per_person_cost = total_cost/4;
+       
+          System.out.println("required fuel in liter = " +required_fuel);
+          System.out.println("Total cost in rs  = " +total_cost);
+          System.out.println("per person cost in rs = " +per_person_cost);
+
+
+
+       }
+
+
+
+ }
